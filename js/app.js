ffc33e937ffa85eb7e4d1e80f4c984dfa36bf654
Andrea Wu <andreaowu@berkeley.edu>
2014-09-15 08:14:12 -0700

    routing

--- js/app.js
+++ js/app.js
@@ -1,23 +1,25 @@
-(function() {
-	var app = angular.module('california', []);
 
-	app.controller('CaliforniaController', function($scope) {
-		$scope.cities = ["Fremont", "San Jose", "Berkeley", "San Francisco"]
-		$scope.places = [
-			{"name": "Mission Peak",
-				"city": "Fremont",
-				"address": "Stanford Ave E, Fremont, CA 94539",
-				"type": "hiking",
-				"description": "2500+ feet in elevation, Mission Peak is a great place to go for an upwards hike",
-				"img": "tbd"},
-			{"name": "Falafel Drive-In",
-				"city": "San Jose",
-				"address": "2301 Stevens Creek Blvd, San Jose, CA 95128",
-				"type": "food",
-				"description": "This place got me to start eating falafels; it's that good! Get the falafel and banana milk shake. It's great!",
-				"img": "tbd"}
-		];
-	});
+/*  app.js  */
 
-})()
+// create the module and name it eweekApp
+var app = angular.module('california', ['ngRoute']);
+
+// configure our routes
+app.config(function($routeProvider) {
+	$routeProvider
+
+		.when('/:filter', {
+			templateUrl : 'pages/all-places.html',
+			controller  : 'PlacesCtrl'
+		}).
+
+		when('/contact', {
+   		    templateUrl: 'pages/contact.html',
+   		    controller: 'ContactCtrl'
+   		}).
+
+		otherwise({
+			redirectTo : ''
+		});
+});
 
