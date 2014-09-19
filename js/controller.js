9f9d45c610247bca1ebeb3bb434248e7092615ef
Andrea Wu <andreaowu@berkeley.edu>
2014-09-19 12:37:42 -0700

    shows correct places

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,6 @@
 app.controller('PlacesCtrl', function($scope, sharedProperties) {
 	$scope.cities = 
-		{"Fremont": ["food", "hiking"],
+		{"Fremont": ["food", "hiking", "outdoors"],
 			"San Jose": ["food", "hiking"], 
 			"Berkeley": ["food", "hiking"], 
 			"San Francisco": ["food", "hiking", "museums", "outdoors"]
@@ -25,7 +25,25 @@ app.controller('PlacesCtrl', function($scope, sharedProperties) {
 			"address": "2301 Stevens Creek Blvd, San Jose, CA 95128",
 			"type": "food",
 			"description": "This place got me to start eating falafels; it's that good! Get the falafel and banana milk shake. It's great!",
-			"img": "tbd"}
+			"img": "tbd"},
+		{"name": "Aberdeen Cafe",
+			"city": "Fremont",
+			"address": "46831 Warm Springs Blvd, Fremont, CA 94539",
+			"type": "food",
+			"description": "Delicious Hong-Kong style food! The wait during lunch is long though, so come early or late. Check out my Yelp review to see what to order!",
+			"img": "tbd"},
+		{"name": "Gong Cha",
+			"city": "Fremont",
+			"address": "46827 Warm Springs Blvd #101, Fremont, CA 94539",
+			"type": "food",
+			"description": "This place has Asian drinks, also known as boba. Must try boba if you're around the area!",
+			"img": "tbd"},
+		{"name": "Central Park",
+			"city": "Fremont",
+			"address": "Lake Elizabeth Fremont, CA 94538",
+			"type": "outdoors",
+			"description": "Central park has many playgrounds, and a lake with a 2-mile path around it. It's a great place to have a picnic, BBQ, or go for a run.",
+			"img": "tbd"},
 	];
 
 	$scope.changeChosen = function(changeTo) {
